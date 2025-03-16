afd62abc0141c322a4d23d960aa191c353ba1b9b
Alex Stevovich <alex.stevovich@gmail.com>
2025-03-15 22:14:43 -0400

    Second Commit

--- scripts/backup.js
+++ scripts/backup.js
@@ -1,8 +1,9 @@
-import { switchBranch, addAndCommitChanges, pushToAllRemotes } from "gitcmds";
+import { merge,  switchBranch, addAndCommitChanges, pushToAllRemotes } from "gitcmds";
 
 
 // Check if --auto flag is present
 const isAuto = process.argv.includes("--auto");
+const toMain = process.argv.includes("--main");
 
 // Construct commit message only if auto mode is enabled
 const commitMessage = isAuto 
@@ -15,3 +16,11 @@ await pushToAllRemotes('dev');
 
 console.log("TEST")
 console.log("✅ Backup process complete.");
+
+
+if(toMain)
+{
+await switchBranch('main')
+await merge("dev");
+await pushToAllRemotes('dev');
+}
